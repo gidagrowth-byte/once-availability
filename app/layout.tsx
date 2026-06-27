22ae7b8ce7d1a94fff0b8c7879a70ec02c740edb
kikuchiamane <kikuchiamane@users.noreply.github.com>
2026-06-27 22:52:38 +0900

    add microsoft clarity script

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata: Metadata = {
 
 const gtmId = "GTM-WFLSD4F2";
 const ga4MeasurementId = "G-460Z0PYTE3";
+const clarityProjectId = "xdlo3x2qaj";
 
 export default function RootLayout({
   children,
@@ -30,6 +31,18 @@ export default function RootLayout({
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${gtmId}');
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                t.onload=function(){console.log("clarity loaded");};
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${clarityProjectId}");
             `,
           }}
         />
